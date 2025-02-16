da1bdaf5bb458f598cc6be0afbc493c2c8e32f41
kjhutao <85632313@qq.com>
2025-02-16 19:53:45 +0800

    2-16

--- test/test.c
+++ test/test.c
@@ -1193,3 +1193,12 @@
 //	
 //	return 0;
 //}
+
+int main(void) {
+	int aa[2][5] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int* ptr1 = (int*)(&aa + 1);
+	int* ptr2 = (int*)(*(aa + 1));
+    printf("%d, %d", *(ptr1 - 1), *(ptr2 - 1));
+
+	return 0;
+}
